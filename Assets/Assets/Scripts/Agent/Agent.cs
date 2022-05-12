21917522d0b7245c3c6a91e3c7cd4ec4c8b14784
bullingbullet <78803026+bullingbullet@users.noreply.github.com>
2022-05-13 00:29:27 +0400

    Add Attack for agents

---
+++ Assets/Assets/Scripts/Agent/Agent.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Agent : MonoBehaviour
+{
+    public GameObject sight;
+    public GameObject agentBullet;
+
+    public Transform player;
+    public Transform agentFirePos;
+
+    public List<Material> agentColors;
+    public List<Transform> points;
+
+    public float fireRate = 0.5f;
+    private float nextFire = 0.0f;
+
+    public NavMeshAgent agent;
+
+    private void Update()
+    {
+        if (Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            GameObject clone = Instantiate(agentBullet, agentFirePos.position, agentFirePos.rotation);
+            Destroy(clone, 1);
+        }
+
+        transform.LookAt(player);
+        
+        if (agent.remainingDistance <= agent.stoppingDistance)
+        {
+            agent.SetDestination(points[Random.Range(0, points.Count)].position);
+        }
+    }
+
+    private void OnMouseEnter()
+    {
+        gameObject.GetComponent<Renderer>().material = agentColors[1];
+    }
+
+    private void OnMouseExit()
+    {
+        gameObject.GetComponent<Renderer>().material = agentColors[0];
+    }
+}
---
+++ Assets/Assets/Scripts/Agent/AgentBullet.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgentBullet : MonoBehaviour
+{
+    Vector3 lastPos;
+
+    public static float punchForce = 900;
+    public float speed;
+
+    private void Start()
+    {
+        lastPos = transform.position;
+    }
+
+    void Update()
+    {
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+
+        RaycastHit hit;
+
+        Debug.DrawLine(lastPos, transform.position);
+        if (Physics.Linecast(lastPos, transform.position, out hit))
+        {
+            if (hit.collider.name == "player")
+            {
+                Destroy(gameObject, 0.3f);
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+            
+        }
+        lastPos = transform.position;
+    }
+}
